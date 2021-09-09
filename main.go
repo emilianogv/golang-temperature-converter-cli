4004cc126775bef88bde64f4391b4718138d857e
Emiliano Gonzalez <emilianogonzalezvaldez@gmail.com>
2021-09-09 15:13:04 -0400

    Add logic to read CLI args

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var originUnit string
@@ -17,6 +18,14 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
+
+	//Do we get the right number of arguments?
+	if len(os.Args) != 2 {
+		printError(errInvalidArguments)
+	}
+
+	//Get the originUnit
+	originUnit = strings.ToUpper(os.Args[1])
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
